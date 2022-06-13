6f558b64852933a6f5201e0ac8088e9c9f2175e5
Jackson <jackson@stbear.com.tw>
2022-06-13 10:48:12 +0800

    vuex設置

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -1,14 +1,62 @@
 import { createStore } from 'vuex'
+import { manager } from '@/services/userDataManager'
 
 export default createStore({
   state: {
+    userInfo: null,
+    token: null,
+    expiryDate: null,
+    isAuthenticated: false,
   },
   getters: {
+    token: state => {
+      return state.token;
+    },
+    isAuthenticated: state => {
+      if (!state.token) {
+        return false;
+      }
+      return state.isAuthenticated;
+    },
   },
   mutations: {
+    /**
+     * 設定使用者資訊
+     * @param {object} state Vuex state物件
+     * @param {object} info 使用者資訊
+     */
+      setUserInfo(state, info) {
+      state.userInfo = Object.assign({}, info);
+      state.token = info.token;
+      state.expiryDate = info.expiryDate;
+      manager.setUserData(info);
+    },
+    /**
+     * 將狀態設為已授權
+     * @param {object} state Vuex state物件
+     */
+    authenticate(state) {
+      state.isAuthenticated = true;
+    },
+    /**
+     * 清空state資料
+     * @param {object} state Vuex state物件
+     */
+    clearUserInfo(state) {
+      state.token = null;
+      state.expiryDate = null;
+      state.isAuthenticated = false;
+      state.userInfo = null;
+      manager.clearData();
+    },
+    /**
+     * 設定當前的clientWidth
+     * @param {object} state Vuex state物件
+     * @param {number} width 當前的Client Width
+     */
+    setClientWidth(state, width) {
+      state.clientWidth = width
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
+  strict: true
 })
