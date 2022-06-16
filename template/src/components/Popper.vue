60d7da38092a28f0c1b189ad6678d2e0ad055c30
Jackson <jackson@stbear.com.tw>
2022-06-16 13:56:53 +0800

    加入彈出視窗組件=>定義Slot 可依情況塞入顯示的內容

---
+++ template/src/components/Popper.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-popover :placement="placement" :width="width" :trigger="trigger">
+    <template #reference>
+      <slot name="trigger"></slot>
+      <!-- <el-icon :class="$style['popper-icon']"><WarningFilled /></el-icon> -->
+    </template>
+    <template #default>
+      <div :class="$style.popper">
+        <el-scrollbar>
+          <slot name="content"></slot>
+          <!-- <a style="text-decoration: none" href="/Login" v-for="(item, index) in gridData" :key="item"><p>test{{ index }}</p></a> -->
+        </el-scrollbar>
+      </div>
+    </template>
+  </el-popover>
+</template>
+
+<script>
+export default {
+  props: {
+    trigger: {
+      type: String,
+      default: 'click'
+    },
+    width: {
+      type: Number,
+      default: 200
+    },
+    placement: {
+      type: String,
+      default: 'bottom'
+    }
+  },
+  setup(props, { emit }) {
+    /*==========Data and Props==========*/
+    const trigger = toRef(props, 'trigger')
+    const width = toRef(props, 'width')
+    const placement = toRef(props, 'placement')
+
+    return { trigger, width, placement }
+  }
+}
+</script>
+
+<style lang="scss" module>
+  @import "@/assets/css/custom.scss";
+</style>
